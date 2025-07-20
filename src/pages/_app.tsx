4b98c96b13c5fb76140b47ffeeb67aedcda37bd2
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2025-07-20 16:48:18 +0900

    chore: emotion, react-query 및 기타 의존성 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Global } from "@emotion/react";
+import globalStyles from "@/styles/globalStyles";
+import { reset } from "@/styles/reset";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Global styles={[reset, globalStyles]} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
